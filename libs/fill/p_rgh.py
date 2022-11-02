973ee42b120e149e218f10a169b3033cbccb2790
IvanSm <81528259+IvanSmirnovSPL@users.noreply.github.com>
2022-11-02 15:52:54 +0300

    Update p_rgh.py

--- libs/fill/p_rgh.py
+++ libs/fill/p_rgh.py
@@ -6,8 +6,6 @@ def fill_p_rgh(params, fn=None, fp=None):
     for B in params.get('boundary_types', None):
         if B == 'wall':
             B = 'wall_pressure'
-        BT.append(B)
-    for B in params.get('boundary_types', None):
         if B == 'in_with_value':
             B = 'in_with_value_pressure'
         BT.append(B)
