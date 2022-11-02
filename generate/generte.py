cc17064d04a86af7571c8416415ea61dfbd60dd4
IvanSm <smirnov.ie@phystech.edu>
2022-11-02 15:41:50 +0300

    make parsing except system/params

--- generate/generte.py
+++ generate/generte.py
@@ -19,7 +19,7 @@ def filePointer(fd=None, fp=None, flag='r'):
         f = fd
     return f
 
-def searchParam(line, d):
+def searchParam(line, d, part=None):
     tmp = line.rfind(';')
     if tmp == -1 or len(line.split()) < 2 or line.split()[0] == 'dimensions':
         return r"'" + line + r"'"
@@ -29,17 +29,25 @@ def searchParam(line, d):
     ed = line.find(')')
     key = lineList[0]
     pattern = re.compile('\$.*')
-    if pattern.match(key)  is not None:
+    if pattern.match(key) is not None:
         print(key)
         key = key[1:]
+    if part is not None:
+        key = part['key'] +f'_{key}'
+    for i in range(20):
+        key = key.replace('.', '')
+        key = key.replace('(', '_')
+        key = key.replace(')', '_')
+        key = key.replace(',', '_')
+        key = key.replace('*', '_')
+        key = key.replace('__', '_')
     if st != -1 and ed != -1:
         default = line[st + 1: ed]
-        line = line[:st] + '({});' + r"'.format(params[" + key + r"])"
+        line = line[:st] + '({});' + r"'.format(params['" + key + r"'])"
     else:
         default = lineList[-1]
-        line = " ".join(lineList[:-1]) + ' {};' + r"'.format(params[" + key + r"])"
+        line = " ".join(lineList[:-1]) + ' {};' + r"'.format(params['" + key + r"'])"
     d[key] = default
-
     return r"r'" + line
 
 def findStars(lines):
@@ -61,13 +69,60 @@ def makeFillFile(rez, fd=None, fp=None):
     bound = findStars(lines)
     for i, line in enumerate(lines):
         if i < bound:
-            print('    ', r"r'" + line[:-1] + r"' + '\n' " + ' + \\', file=rez)
+            print('    ', r"r'" + line[:-1] + r"' + '\n' " + '+ \\', file=rez)
             continue
         line = searchParam(line[:-1], d)
         if i == len(lines) - 1:
             print('    ', line + r" + '\n '", file=rez)
         else:
-            print('    ', line + r" + '\n' " + ' + \\', file=rez)
+            print('    ', line + r" + '\n' " + '+ \\', file=rez)
+    rez.close()
+    return (d)
+
+def findParts(lines):
+    pattern1 = re.compile('.*\{.*')
+    pattern2 = re.compile('.*\}.*')
+    rez = []
+    flag = False
+    for i, line in enumerate(lines):
+        if pattern1.match(line) is not None:
+            tmp = {}
+            tmp['key'] = lines[i - 1].split()[0]
+            tmp['lbound'] = i
+            flag = True
+        if flag:
+            if pattern2.match(line) is not None:
+                tmp['rbound'] = i
+                rez.append(tmp)
+                flag = False
+    return rez
+
+
+
+def makeFillDuplicateNames(rez, fd=None, fp=None):
+    f = filePointer(fd=fd, fp=fp)
+    rez = filePointer(fd=rez, flag='w')
+    lines = f.readlines()
+    f.close()
+    d = {}
+    lines = lines[7:]
+    bound = findStars(lines)
+    parts = findParts(lines)
+    for i, line in enumerate(lines):
+        if i < bound:
+            print('    ', r"r'" + line[:-1] + r"' + '\n' " + '+ \\', file=rez)
+            continue
+        if i == len(lines) - 1:
+            line = searchParam(line[:-1], d)
+            print('    ', line + r" + '\n '", file=rez)
+        else:
+            part = None
+            for foo in parts:
+                if i >= foo['lbound'] and i <= foo['rbound']:
+                    part = foo
+                    break
+            line = searchParam(line[:-1], d, part)
+            print('    ', line + r" + '\n' " + '+ \\', file=rez)
     rez.close()
     return (d)
 
@@ -88,10 +143,13 @@ def makeFiles(src, dist):
             path = Path(dist,name)
             rez = open(f'{path}.py', 'w')
             print(f'def fill_{name}(params):', '    return ', sep='\n', end='', file=rez)
-            dd = makeFillFile(rez=rez, fp=filename)
+            if name == 'fvSchemes' or name == 'fvSolution':
+                dd = makeFillDuplicateNames(rez=rez, fp=filename)
+            else:
+                dd = makeFillFile(rez=rez, fp=filename)
             d[name] = dd
     return d
 
 
 d = makeFiles(src, dist)
-print(d)
+print(d['fvSchemes'].keys())
