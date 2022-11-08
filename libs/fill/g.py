608d9dfeced0d914c67e134eb557efa2c9b98272
IvanSm <81528259+IvanSmirnovSPL@users.noreply.github.com>
2022-11-08 13:51:58 +0300

    Delete libs directory

--- libs/fill/g.py
+++
@@ -1,4 +0,0 @@
-def fill_g(params):
-    return '  ' + '\n' + \
-           ' dimensions      {}; '.format(params['dimensions_g']) + '\n' + \
-           ' value           ({}); '.format(params['g'])
--- libs/fill/turbulenceProperties.py
+++
@@ -1,3 +0,0 @@
-def fill_turbulenceProperties(params):
-    return '  ' + '\n' + \
-           ' simulationType {}; '.format(params['simulationType']) + '\n'
