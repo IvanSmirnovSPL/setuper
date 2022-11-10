126a1366ab5a3ed64a306260315331b1f5d70765
IvanSm <smirnov.ie@phystech.edu>
2022-11-10 21:39:15 +0300

    init

--- libs/fill/thermophysicalProperties.py
+++ libs/fill/thermophysicalProperties.py
@@ -1,90 +1,88 @@
 def fill_thermophysicalProperties(params):
-    return  '' + '\n' + \
-     r'phases ({});'.format(params['phases']) + '\n' + \
-     '' + '\n' + \
+    return      '' + '\n' + \
      r'properties_rhoe ({});'.format(params['properties_rhoe']) + '\n' + \
      r'properties_TP ({});'.format(params['properties_TP']) + '\n' + \
      r'properties_TRho ({});'.format(params['properties_TRho']) + '\n' + \
      '' + '\n' + \
-     'rhoe_setting' + '\n' + \
+     'rhoe_settings' + '\n' + \
+     '{' + '\n' + \
+     '    vapor_mass_fraction' + '\n' + \
+     '    {' + '\n' + \
+     r'name {};'.format(params['name']) + '\n' + \
+     r'type {};'.format(params['type']) + '\n' + \
+     '    }' + '\n' + \
+     '    temperature' + '\n' + \
+     '    {' + '\n' + \
+     r'name {};'.format(params['name']) + '\n' + \
+     r'type {};'.format(params['type']) + '\n' + \
+     '    }' + '\n' + \
+     '    pressure' + '\n' + \
+     '    {' + '\n' + \
+     r'name {};'.format(params['name']) + '\n' + \
+     r'type {};'.format(params['type']) + '\n' + \
+     '    }' + '\n' + \
+     '    speed_of_sound' + '\n' + \
+     '    {' + '\n' + \
+     r'name {};'.format(params['name']) + '\n' + \
+     r'type {};'.format(params['type']) + '\n' + \
+     '    }' + '\n' + \
+     '    viscosity' + '\n' + \
+     '    {' + '\n' + \
+     r'name {};'.format(params['name']) + '\n' + \
+     r'type {};'.format(params['type']) + '\n' + \
+     '    }' + '\n' + \
+     '    thermal_conductivity' + '\n' + \
+     '    {' + '\n' + \
+     r'name {};'.format(params['name']) + '\n' + \
+     r'type {};'.format(params['type']) + '\n' + \
+     '    }' + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
-     '    vapor_mass_fractio' + '\n' + \
-     '    ' + '\n' + \
-     r'name {};'.format(params['name']) + '\n' + \
-     r'type {};'.format(params['type']) + '\n' + \
-     '    ' + '\n' + \
-     '    temperatur' + '\n' + \
-     '    ' + '\n' + \
-     r'name {};'.format(params['name']) + '\n' + \
-     r'type {};'.format(params['type']) + '\n' + \
-     '    ' + '\n' + \
-     '    pressur' + '\n' + \
-     '    ' + '\n' + \
-     r'name {};'.format(params['name']) + '\n' + \
-     r'type {};'.format(params['type']) + '\n' + \
-     '    ' + '\n' + \
-     '    speed_of_soun' + '\n' + \
-     '    ' + '\n' + \
-     r'name {};'.format(params['name']) + '\n' + \
-     r'type {};'.format(params['type']) + '\n' + \
-     '    ' + '\n' + \
-     '    viscosit' + '\n' + \
-     '    ' + '\n' + \
-     r'name {};'.format(params['name']) + '\n' + \
-     r'type {};'.format(params['type']) + '\n' + \
-     '    ' + '\n' + \
-     '    thermal_conductivit' + '\n' + \
-     '    ' + '\n' + \
-     r'name {};'.format(params['name']) + '\n' + \
-     r'type {};'.format(params['type']) + '\n' + \
-     '    ' + '\n' + \
-     '' + '\n' + \
-     '' + '\n' + \
-     'TP_setting' + '\n' + \
-     '' + '\n' + \
-     '    vapor_mass_fractio' + '\n' + \
-     '    ' + '\n' + \
+     'TP_settings' + '\n' + \
+     '{' + '\n' + \
+     '    vapor_mass_fraction' + '\n' + \
+     '    {' + '\n' + \
      r'name {};'.format(params['name']) + '\n' + \
      r'type {};'.format(params['type']) + '\n' + \
      r'mixture_calculation_method {};'.format(params['mixture_calculation_method']) + '\n' + \
-     '    ' + '\n' + \
-     '    densit' + '\n' + \
-     '    ' + '\n' + \
+     '    }' + '\n' + \
+     '    density' + '\n' + \
+     '    {' + '\n' + \
      r'name {};'.format(params['name']) + '\n' + \
      r'type {};'.format(params['type']) + '\n' + \
      r'mixture_calculation_method {};'.format(params['mixture_calculation_method']) + '\n' + \
-     '    ' + '\n' + \
-     '    internal_energ' + '\n' + \
-     '    ' + '\n' + \
+     '    }' + '\n' + \
+     '    internal_energy' + '\n' + \
+     '    {' + '\n' + \
      r'name {};'.format(params['name']) + '\n' + \
      r'type {};'.format(params['type']) + '\n' + \
      r'mixture_calculation_method {};'.format(params['mixture_calculation_method']) + '\n' + \
-     '    ' + '\n' + \
-     '    speed_of_soun' + '\n' + \
-     '    ' + '\n' + \
+     '    }' + '\n' + \
+     '    speed_of_sound' + '\n' + \
+     '    {' + '\n' + \
      r'name {};'.format(params['name']) + '\n' + \
      r'type {};'.format(params['type']) + '\n' + \
      r'mixture_calculation_method {};'.format(params['mixture_calculation_method']) + '\n' + \
-     '    ' + '\n' + \
-     '    viscosit' + '\n' + \
-     '    ' + '\n' + \
+     '    }' + '\n' + \
+     '    viscosity' + '\n' + \
+     '    {' + '\n' + \
      r'name {};'.format(params['name']) + '\n' + \
      r'type {};'.format(params['type']) + '\n' + \
      r'mixture_calculation_method {};'.format(params['mixture_calculation_method']) + '\n' + \
-     '    ' + '\n' + \
-     '    thermal_conductivit' + '\n' + \
-     '    ' + '\n' + \
+     '    }' + '\n' + \
+     '    thermal_conductivity' + '\n' + \
+     '    {' + '\n' + \
      r'name {};'.format(params['name']) + '\n' + \
      r'type {};'.format(params['type']) + '\n' + \
      r'mixture_calculation_method {};'.format(params['mixture_calculation_method']) + '\n' + \
-     '    ' + '\n' + \
+     '    }' + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
-     '' + '\n' + \
-     'TRho_setting' + '\n' + \
-     '' + '\n' + \
-     '    vapor_volume_fractio' + '\n' + \
-     '    ' + '\n' + \
+     'TRho_settings' + '\n' + \
+     '{' + '\n' + \
+     '    vapor_volume_fraction' + '\n' + \
+     '    {' + '\n' + \
      r'name {};'.format(params['name']) + '\n' + \
      r'type {};'.format(params['type']) + '\n' + \
-     '    ' + '\n' + \
-     '' + '\n '
+     '    }' + '\n' + \
+     '}' + '\n '
--- libs/fill/turbulenceProperties.py
+++ libs/fill/turbulenceProperties.py
@@ -1,14 +1,6 @@
 def fill_turbulenceProperties(params):
-    return      r'FoamFile' + '\n' + \
-     r'{' + '\n' + \
-     r'    version     2.0;' + '\n' + \
-     r'    format      ascii;' + '\n' + \
-     r'    class       dictionary;' + '\n' + \
-     r'    object      turbulenceProperties;' + '\n' + \
-     r'}' + '\n' + \
-     r'// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //' + '\n' + \
-     '' + '\n' + \
+    return      '' + '\n' + \
      r'simulationType {};'.format(params['simulationType']) + '\n' + \
      '' + '\n' + \
      '' + '\n' + \
-     '// ************************************************************************* /' + '\n '
+     '// ************************************************************************* //' + '\n '
