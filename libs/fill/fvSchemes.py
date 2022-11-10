2a0caeb634af7fd3a2d4c59ae2f40a4683945bbf
IvanSm <smirnov.ie@phystech.edu>
2022-11-10 17:46:50 +0300

    init

--- libs/fill/fvSchemes.py
+++ libs/fill/fvSchemes.py
@@ -8,48 +8,48 @@ def fill_fvSchemes(params):
      r'    object      fvSchemes;' + '\n' + \
      r'}' + '\n' + \
      r'// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //' + '\n' + \
-     'compressibl' + '\n' + \
-     '' + '\n' + \
+     'compressible' + '\n' + \
+     '{' + '\n' + \
      r'fluxScheme {};'.format(params['compressible_fluxScheme']) + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
-     '' + '\n' + \
-     'ddtScheme' + '\n' + \
-     '' + '\n' + \
+     'ddtSchemes' + '\n' + \
+     '{' + '\n' + \
      r'default {};'.format(params['ddtSchemes_default']) + '\n' + \
      r'fluxIntegrator {};'.format(params['ddtSchemes_fluxIntegrator']) + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
-     '' + '\n' + \
-     'gradScheme' + '\n' + \
-     '' + '\n' + \
+     'gradSchemes' + '\n' + \
+     '{' + '\n' + \
      r'default Gauss {};'.format(params['gradSchemes_default']) + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
-     '' + '\n' + \
-     'divScheme' + '\n' + \
-     '' + '\n' + \
+     'divSchemes' + '\n' + \
+     '{' + '\n' + \
      r'default {};'.format(params['divSchemes_default']) + '\n' + \
      r'    div({});'.format(params['divSchemes_div_tauMC_']) + '\n' + \
      r'    div(({});'.format(params['divSchemes_div_SU_']) + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
-     '' + '\n' + \
-     'laplacianScheme' + '\n' + \
-     '' + '\n' + \
+     'laplacianSchemes' + '\n' + \
+     '{' + '\n' + \
      r'default Gauss linear {};'.format(params['laplacianSchemes_default']) + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
-     '' + '\n' + \
-     'interpolationScheme' + '\n' + \
-     '' + '\n' + \
+     'interpolationSchemes' + '\n' + \
+     '{' + '\n' + \
      r'default {};'.format(params['interpolationSchemes_default']) + '\n' + \
      r'scheme {};'.format(params['interpolationSchemes_scheme']) + '\n' + \
-     '   ' + '\n' + \
+     '    ' + '\n' + \
      r'    reconstruct({});'.format(params['interpolationSchemes_reconstruct_thermorho_']) + '\n' + \
      r'    reconstruct({});'.format(params['interpolationSchemes_reconstruct_rhoU_']) + '\n' + \
      r'    reconstruct({});'.format(params['interpolationSchemes_reconstruct_rhoE_']) + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
-     '' + '\n' + \
-     'snGradScheme' + '\n' + \
-     '' + '\n' + \
+     'snGradSchemes' + '\n' + \
+     '{' + '\n' + \
      r'default {};'.format(params['snGradSchemes_default']) + '\n' + \
-     '' + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
      '' + '\n' + \
      '// ************************************************************************* /' + '\n '
--- libs/fill/fvSolution.py
+++ libs/fill/fvSolution.py
@@ -9,28 +9,28 @@ def fill_fvSolution(params):
      r'}' + '\n' + \
      r'// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //' + '\n' + \
      '' + '\n' + \
-     'solver' + '\n' + \
-     '' + '\n' + \
-     '    ' + '\n' + \
-     '    ' + '\n' + \
+     'solvers' + '\n' + \
+     '{' + '\n' + \
+     '    E' + '\n' + \
+     '    {' + '\n' + \
      r'solver {};'.format(params['E_solver']) + '\n' + \
      r'preconditioner {};'.format(params['E_preconditioner']) + '\n' + \
      r'tolerance {};'.format(params['E_tolerance']) + '\n' + \
      r'relTol {};'.format(params['E_relTol']) + '\n' + \
-     '    ' + '\n' + \
-     '    ' + '\n' + \
-     '    ' + '\n' + \
+     '    }' + '\n' + \
+     '    U' + '\n' + \
+     '    {' + '\n' + \
      r'solver {};'.format(params['U_solver']) + '\n' + \
      r'preconditioner {};'.format(params['U_preconditioner']) + '\n' + \
      r'tolerance {};'.format(params['U_tolerance']) + '\n' + \
      r'relTol {};'.format(params['U_relTol']) + '\n' + \
-     '    ' + '\n' + \
+     '    }' + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
-     '' + '\n' + \
-     'SIMPL' + '\n' + \
-     '' + '\n' + \
+     'SIMPLE' + '\n' + \
+     '{' + '\n' + \
      r'nNonOrthogonalCorrectors {};'.format(params['SIMPLE_nNonOrthogonalCorrectors']) + '\n' + \
-     '' + '\n' + \
+     '}' + '\n' + \
      '' + '\n' + \
      '' + '\n' + \
      '// ************************************************************************* /' + '\n '
